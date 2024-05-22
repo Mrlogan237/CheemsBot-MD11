25e1712fc682d6db37c77803ee7ffc2a962389e0
Mrlogan237 <114101991+Mrlogan237@users.noreply.github.com>
2024-05-23 00:29:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,19 +19,19 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = '𝐈𝛕ᷟ𝚣⃪ꙴ ͢🫧🅻𝗢𝗚𝗔🅽𝆺𝅥⃝ 🇬 𝗢𝗗🥀' //ur bot name
+global.ownernumber = '237670507356' //ur owner number
+global.ownername = '𝐈𝛕ᷟ𝚣⃪ꙴ ͢🫧🅻𝗢𝗚𝗔🅽𝆺𝅥⃝🥀' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
+global.themeemoji = '✮'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "𝐈𝛕ᷟ𝚣⃪ꙴ ͢🫧🅻𝗢𝗚𝗔🅽𝆺𝅥⃝ 🇬 𝗢𝗗🥀\n\nDonne ta vie au seigneur ITACHI "
+global.creator = "237670507356@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["237670507356"] // Premium User
 global.hituet = 0
 
 //bot sett
